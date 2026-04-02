544325719fd8121556bb46016fd99bb963932198
LangstonW430 <langstonw430@gmail.com>
2026-04-02 14:28:57 -0400

    correcting the analytics import

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import headshotImg from "./assets/Enhanced Headshot.jpg";
 import resumePDF from "./assets/CS_Resume.pdf";
 import "./App.css";
